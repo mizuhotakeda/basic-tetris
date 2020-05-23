bb00ae9e7823e5e82134eaf51e9314517077eb73
mizuhotakeda <32306596+mizuhotakeda@users.noreply.github.com>
2020-05-23 16:45:23 +0200

    Part1

---
+++ app.js
@@ -0,0 +1,132 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('start-button')
+    const width = 10
+
+    //The Tetrominoes
+    const lTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ]
+
+    const zTetromino = [
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1],
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1]
+    ]
+
+    const tTetromino = [
+        [1, width, width+1, width+2],
+        [1, width+1, width+2, width*2+1],
+        [width,width+1, width+2, width*2+1],
+        [1, width, width+1, width*2+1]
+    ]
+
+    const oTetromino = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
+    ]
+
+    const iTetromino = [
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3]
+    ]
+
+    const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+    let currentPosition = 4
+    let currentRotation = 0
+
+    //randomly selecte a Tetromino and its first rotation
+    let random = Math.floor(Math.random() * theTetrominoes.length)
+    console.log(random)
+    let current = theTetrominoes[random][currentRotation]
+
+    //draw the tetromino
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('tetromino')
+        })
+    }
+
+    //undraw the tetromino
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino')
+        })
+    }
+
+    //make the tetromino move down every second
+    timerId = setInterval(moveDown, 1000)
+
+    //keyCodes
+    function control(e) {
+        if(e.keyCode === 37) {
+            moveLeft()
+        } else if(e.keyCode === 38) {
+            rotate()
+        } else if(e.keyCode === 39) {
+            moveRight()
+        } else if(e.keyCode === 40) {
+            moveDown()
+        }
+    }
+    document.addEventListener('keyup', control)
+
+    //move down
+    function moveDown() {
+        undraw()
+        currentPosition += width
+        draw()
+        freeze()
+    }
+
+    //freeze
+    function freeze() {
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+            //start a new tetromino falling
+            random = Math.floor(Math.random() * theTetrominoes.length)
+            current = theTetrominoes[random][currentRotation]
+            currentPosition = 4
+            draw()
+        }
+    }
+
+    //the tetromino is at the edge or there is a blockage
+    function moveLeft() {
+        undraw()
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+        if(!isAtLeftEdge) currentPosition -=1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition +=1
+        }
+
+        draw()
+    }
+
+    function moveRight() {
+        undraw()
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1)
+
+        if(!isAtRightEdge) currentPosition +=1 
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition -=1
+        }
+
+        draw()
+    }
+
+})
